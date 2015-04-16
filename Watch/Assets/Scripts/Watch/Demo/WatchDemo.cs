fb1a1002abe6dbc222f2ef0e02fce6233a41a343
Prokhor Kharchenko <harchenko@mlsdev.com>
2015-04-16 11:13:36 +0300

    basic 'Watch'  implementation

---
+++ Watch/Assets/Scripts/Watch/Demo/WatchDemo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class WatchDemo : MonoBehaviour {
+
+	void Start () {
+		Watch.Log ("start log", Time.time.ToString());
+	}
+	
+	void Update () {
+		Watch.Log ("update log", Time.timeSinceLevelLoad.ToString());
+	}
+}
---
+++ Watch/Assets/Scripts/Watch/Watch.cs
@@ -0,0 +1,10 @@
+﻿public static class Watch {
+	[System.Diagnostics.Conditional("DEBUG"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+	public static void Log(string key, string message) {
+		if (string.IsNullOrEmpty (key))
+			return;
+
+		WatchContext.instance.AddMessage(key, message);
+	}
+
+}
