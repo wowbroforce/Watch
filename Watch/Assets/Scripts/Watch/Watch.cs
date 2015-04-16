8ee41f4acadcc2bcf62e19a96dc8fa4e31b39f1c
Prokhor Kharchenko <harchenko@mlsdev.com>
2015-04-16 17:17:54 +0300

    using watched objects in context

--- Watch/Assets/Scripts/Watch/Watch.cs
+++ Watch/Assets/Scripts/Watch/Watch.cs
@@ -1,4 +1,6 @@
-﻿public static class Watch {
+﻿using System;
+
+public static class Watch {
     [System.Diagnostics.Conditional("DEBUG"), System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void Log(string key, string message) {
         if (string.IsNullOrEmpty(key))
@@ -6,4 +8,13 @@
 
         WatchContext.instance.AddMessage(key, message);
     }
+
+	[System.Diagnostics.Conditional("DEBUG"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+	public static void Add(object target, Func<object, string> describe) {
+		if (target == null) {
+			return;
+		}
+
+		WatchContext.instance.AddObject (target, describe);
+	}
 }
--- Watch/Assets/Scripts/Watch/WatchContext.cs
+++ Watch/Assets/Scripts/Watch/WatchContext.cs
@@ -1,33 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 
-public class WatchContext : ScriptableObject {
-    public static WatchContext instance {
-        get {
-            if (_instance == null) {
-                _instance = Resources.FindObjectsOfTypeAll<WatchContext>().FirstOrDefault();
-                if (_instance == null) {
-                    _instance = CreateInstance<WatchContext>();
-                    _instance.hideFlags = HideFlags.DontSave;
-                }
-            }
-            return _instance;
-        }
-    }
+public class WatchContext : ScriptableObject
+{
+	public static WatchContext instance {
+		get {
+			if (_instance == null) {
+				_instance = Resources.FindObjectsOfTypeAll<WatchContext> ().FirstOrDefault ();
+				if (_instance == null) {
+					_instance = CreateInstance<WatchContext> ();
+					_instance.hideFlags = HideFlags.DontSave;
+				}
+			}
+			return _instance;
+		}
+	}
 
-    private static WatchContext _instance;
-    private readonly Dictionary<string, string> _messages = new Dictionary<string, string>();
+	private static WatchContext _instance;
+	private readonly Dictionary<string, string> _messages = new Dictionary<string, string> ();
+	private readonly List<WatchTuple<WeakReference, Func<object, string>>> _objects = new List<WatchTuple<WeakReference, Func<object, string>>> ();
 
-    public void AddMessage(string key, string value) {
-        _messages[key] = value;
-    }
+	public void AddMessage (string key, string value)
+	{
+		_messages [key] = value;
+	}
 
-    public void ForEach(WatchContextForEachDelegate action) {
-        foreach (var message in _messages) {
-            action(message.Key, message.Value);
-        }
-    }
+	public void AddObject (object target, Func<object, string> describe) {
+
+		if (_objects.Where (o => o.item1.IsAlive).Select (o => o.item1.Target).Contains (target)) {
+			return;
+		}
+
+		_objects.Add (WatchTuple.Create(new WeakReference(target), describe));
+	}
+
+	public void ForEach (WatchContextForEachDelegate action)
+	{
+//		foreach (var message in _messages) {
+//			action (message.Key, message.Value);
+//		}
+		foreach (var obj in _objects) {
+			if (obj.item1.IsAlive) {
+				var target = obj.item1.Target;
+				action (target.ToString (), obj.item2(target));
+			}
+		}
+	}
+
+	private void UpdateObjects() {
+		_objects.RemoveAll (o => !o.item1.IsAlive);
+	}
+
+	private void OnEnable() {
+		UpdateObjects ();
+	}
 }
 
-public delegate void WatchContextForEachDelegate(string key, string value);
+public delegate void WatchContextForEachDelegate (string key,string value);
